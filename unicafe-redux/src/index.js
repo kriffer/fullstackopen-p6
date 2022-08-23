90f4854e184960e845b265303953757f5f791472
kriffer <antonio.kravets@gmail.com>
2022-08-23 20:46:57 +0300

    6.1-6.8

--- unicafe-redux/src/index.js
+++ unicafe-redux/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import reducer from './reducer'
 
-const store = createStore(reducer)
+const store = configureStore({reducer})
 
 const App = () => {
   const good = () => {
